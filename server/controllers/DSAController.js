97a7d88a94e2c8ad08e70a4d07f28ac15753ebc4
Dhruv Jain <dhruvjain0412@gmail.com>
2026-08-24 00:09:11 +0530

    feat: implement CRUD operations for DSA problems in DSAController and update DSARouter

--- server/controllers/DSAController.js
+++ server/controllers/DSAController.js
@@ -33,4 +33,58 @@ async function createProblem(req, res) {
   }
 }
 
-export { createProblem };
+async function getAllProblems(req, res) {
+  try {
+    const problems = await prisma.dSAProblem.findMany({
+      where: { userId: req.userId },
+    });
+
+    res.json(problems);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+async function getProblem(req, res) {
+  const id = req.params.id;
+  try {
+    const problem = await prisma.dSAProblem.findUnique({
+      where: { id: Number(id) },
+    });
+
+    res.json(problem);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+async function updateProblem(req, res) {
+  const id = Number(req.params.id);
+  try {
+    const problem = await prisma.dSAProblem.update({
+      where: { id },
+      data: req.body,
+    });
+    res.status(200).json(problem);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+async function deleteProblem(req, res) {
+  const id = Number(req.params.id);
+  try {
+    let deletedProblem = await prisma.dSAProblem.delete({ where: { id } });
+    res.status(200).send("Problem Deleted", deleteProblem);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export {
+  createProblem,
+  getAllProblems,
+  getProblem,
+  updateProblem,
+  deleteProblem,
+};
--- server/routes/DSARouter.js
+++ server/routes/DSARouter.js
@@ -1,9 +1,19 @@
 import express from "express";
 import isLoggedIn from "../middleware/isLoggedIn.js";
-import { createProblem } from "../controllers/DSAController.js";
+import {
+  createProblem,
+  getAllProblems,
+  getProblem,
+  updateProblem,
+  deleteProblem,
+} from "../controllers/DSAController.js";
 
 const router = express.Router();
 
-router.post("/add", isLoggedIn, createProblem);
+router.post("/", isLoggedIn, createProblem);
+router.get("/", isLoggedIn, getAllProblems);
+router.get("/:id", isLoggedIn, getProblem);
+router.patch("/:id", isLoggedIn, updateProblem);
+router.delete("/:id", isLoggedIn, deleteProblem);
 
 export default router;
