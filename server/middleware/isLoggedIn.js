0e6dfe9c9ed8302b2d559ccc05ac47afa6045eaa
Dhruv Jain <dhruvjain0412@gmail.com>
2026-08-23 22:46:54 +0530

    feat: add cookie-parser dependency, implement isLoggedIn middleware, and create DSAController and DSARouter files

---
+++ server/middleware/isLoggedIn.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+function isLoggedIn(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Something Went Wrong!" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: "Something Went Wrong!" });
+  }
+}
+
+export default isLoggedIn;
