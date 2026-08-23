973f7edff5c678bb790cc16adf34ff6ca7e1f1a3
Dhruv Jain <dhruvjain0412@gmail.com>
2026-08-23 22:32:05 +0530

    feat: implement user registration and login functionality with JWT authentication

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,18 +1,53 @@
 import { prisma } from "../lib/prisma.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 async function registerUser(req, res) {
   try {
     const { email, name, password } = req.body;
+
+    const checkUser = await prisma.user.findUnique({ where: { email } });
+
+    if (checkUser) {
+      return res.status(400).json({ error: "Account already exists" });
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+
     const user = await prisma.user.create({
       data: {
         email,
         name,
-        password,
+        password: hash,
       },
     });
+
     return res.status(201).json({ message: "User Created Successfully", user });
   } catch (error) {
     return res.status(400).json({ message: error.message });
+  }
+}
+
+async function loginUser(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    const checkUser = await prisma.user.findUnique({ where: { email } });
+
+    if (!checkUser) {
+      return res.status(401).send("Invalid Email or Password");
+    }
+
+    if (await bcrypt.compare(password, checkUser.password)) {
+      const token = jwt.sign({ userId: checkUser.id }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+      return res.status(200).json({ message: "Login successful", token });
+    } else {
+      return res.status(401).send("Invalid Email or Password");
+    }
+  } catch (error) {
+    return res.status(401).send("Something Went Wrong");
   }
 }
 
@@ -25,4 +60,4 @@ async function getAllUsers(req, res) {
   }
 }
 
-export { registerUser, getAllUsers };
+export { registerUser, getAllUsers, loginUser };
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { registerUser, getAllUsers } from "../controllers/userController.js";
+import {
+  registerUser,
+  getAllUsers,
+  loginUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.post("/", registerUser);
-router.get("/", getAllUsers);
+router.post("/register", registerUser);
+router.get("/getAllUsers", getAllUsers);
+router.post("/login", loginUser);
 
 export default router;
